c2a5d4373a25cbf7b9e0959e8e40ed74479a3cff
vishal-770 <vishalpeace07@gmail.com>
2026-09-12 17:22:20 +0530

    feat(auth): add user 6aa53bfae482469c59899ecd to designated admin IDs

--- src/config/admins.ts
+++ src/config/admins.ts
@@ -6,6 +6,7 @@ export const ADMIN_USER_IDS: string[] = [
   "6aa41ebcf8e6a5333a46830d", // Vishal P
   "6aa518cfe0b081a8f0f759a6", // FRANK
   "6aa436d32df875cca8324cd8", // Supbase test
+  "6aa53bfae482469c59899ecd",
   ...(process.env.ADMIN_USER_IDS
     ? process.env.ADMIN_USER_IDS.split(",")
         .map((s) => s.trim())
